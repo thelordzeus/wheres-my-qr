cbbada627d0f370dd7577f2735e49c99dae57268
thelordzeus <arnav55125@gmail.com>
2023-01-31 20:53:52 +0530

    fix: Qr code now generating

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
   const urlElement = document.getElementById("url");
   urlElement.innerHTML = url;
 
-  const generateBtn = document.querySelector("#generateBtn");
   const qrImg = document.querySelector("#qrImg");
-
-  generateBtn.addEventListener("click", () => {
-    if (!url) return;
-    console.log("Button clicked");
-    generateBtn.innerHTML = "Generating QR code";
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`;
-    qrImg.addEventListener("load", () => {
-      generateBtn.innerHTML = "Generate QR code";
-    });
-  });
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`;
 });
