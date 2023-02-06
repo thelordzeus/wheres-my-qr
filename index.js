44c421faec1eefe960c8279e43115a569829556e
thelordzeus <arnav55125@gmail.com>
2023-02-06 19:40:39 +0530

    minor fix

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`;
 });
 
+// why this button not working idk
+
 const BUTTON = document.querySelector("button");
 const IMG = document.querySelector("img");
 
